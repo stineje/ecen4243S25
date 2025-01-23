495cf5787cfd4fe6648246c8e9d46a768d873de8
onyx-93 <143036862+onyx-93@users.noreply.github.com>
2025-01-22 19:09:48 -0600

    reg file completed need tb

--- lab0/SV/tb_regfile.sv
+++ lab0/SV/tb_regfile.sv
@@ -5,71 +5,57 @@
 // james.stine@okstate.edu  28 August 2023
 // Oklahoma State University
 
-module stimulus;
+module stimulus ();
 
-   logic [31:0] a;   
-   logic [31:0] b;   
-   logic [31:0] z;
+   logic clk;
+   logic We3;
+   logic [4:0] Ra1, Ra2, Wa3;
+   logic [31:0] Wd3, Rd1, Rd2;   
 
-   logic [31:0] z_correct;
-   
-   logic 	clk;
-   logic [31:0] errors;
-   logic [31:0] vectornum;
+   regfile dut (clk, We3, Ra1, Ra2, Wa3, Wd3, Rd1, Rd2);
 
-   // Set number of tests
-   localparam NUM_OF_TESTS = 64;   
-   
-   integer 	 handle3;
-   integer 	 i;  
-   integer       j;
-   integer 	 y_integer;   
-   integer 	 sum; 
-
-   sample dut (a, b, z);
-
-   // 1 ns clock
+   // Setup the clock to toggle every 1 time units 
    initial 
      begin	
-	clk = 1'b1;
-	forever #5 clk = ~clk;
+	clock = 1'b1;
+	forever #5 clock = ~clock; // to toggle the clock every 5 time units. This creates a clock with a 10 time-unit period.
      end
 
    initial
      begin
-	// Output file used for fdisplay
-	handle3 = $fopen("sample.out");
-	vectornum = 0;
-	errors = 0;		
+	// Gives output file name
+	handle3 = $fopen("test.out");
+	// Tells when to finish simulation
+	#500 $finish;		
      end
 
-   initial
+   always 
      begin
-	for (j=0; j < NUM_OF_TESTS; j=j+1)
-	  begin
-	     // Put vectors before beginning of clk
-	     @(posedge clk)
-	       begin
-		  a = $random;
-		  b = $random;
-	       end
-	     // Test output at neg edge of clk
-	     @(negedge clk)
-	       begin
-		  z_correct = a+b;
-		  vectornum = vectornum + 1;		       
-		  if (z_correct != z)
-		    begin
-		       errors = errors + 1;
-		       $display("%h %h || %h %h", 
-				a, b, z, z_correct);
-		    end		       
-		  #0 $fdisplay(handle3, "%h %h || %h %h %b", 
-			       a, b, z, z_correct, (z == z_correct));
-	       end // @(negedge clk)		  
-	  end 
-	$display("%d tests completed with %d errors", vectornum, errors);
-	$finish;	
-     end // initial begin   
+	desc3 = handle3;
+	#5 $fdisplay(desc3, "%b %b || %b", 
+		     reset, In, Out);
+     end   
+   
+   initial 
+     begin      
+	     #0 In = 1'b1;
+          #0 reset = 1'b0;
+          #5 reset = 1'b1;  // Assert reset at time 5
+          #10 reset = 1'b0;  // De-assert reset after 15 time units (FSM starts from S0)
+
+          // Tests design with input sequence of 01_0011_0111 
+          #0 In = 1'b0;  // Apply input 0 (should move FSM from S0 to S1)
+          #10 In = 1'b1; // Apply input 1 (should move FSM from S1 to S2)
+          #10 In = 1'b0; // Apply input 0 (should move FSM from S2 to S1)
+          #10 In = 1'b0; // Apply input 0 (should move FSM from S1 to S1)
+          #10 In = 1'b1; // Apply input 1 (should move FSM from S1 to S2)
+          #10 In = 1'b1; // Apply input 1 (should move FSM from S2 to S0)
+          #10 In = 1'b0; // Apply input 0 (should move FSM from S0 to S1)
+          #10 In = 1'b1; // Apply input 1 (should move FSM from S1 to S2)
+          #10 In = 1'b1; // Apply input 1 (should move FSM from S2 to S0)
+          #10 In = 1'b1; // Apply input 1 (should move FSM from S0 to S0)
+
+
+     end
 
 endmodule // stimulus
