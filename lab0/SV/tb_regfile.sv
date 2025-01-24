c604b4f6ee2127f1fc084730ecefc8a5272704b6
onyx-93 <143036862+onyx-93@users.noreply.github.com>
2025-01-24 13:33:54 -0600

    updated

--- lab0/SV/tb_regfile.sv
+++ lab0/SV/tb_regfile.sv
@@ -1,29 +1,34 @@
 `timescale 1ns/1ps
 
-// Sample Testbench to display verification
-// James E. Stine
-// james.stine@okstate.edu  28 August 2023
-// Oklahoma State University
-
 module stimulus ();
 
    logic clk;
-   logic We3;
-   logic [4:0] Ra1, Ra2, Wa3;
-   logic [31:0] Wd3, Rd1, Rd2;   
+   logic we3;
+   logic [4:0] ra1, ra2, wa3;
+   logic [31:0] wd3;
+   logic [31:0] rd1, rd2;   
 
-   regfile dut (clk, We3, Ra1, Ra2, Wa3, Wd3, Rd1, Rd2);
+   regfile dut (clk, we3, ra1, ra2, wa3, wd3, rd1, rd2);
 
    // Setup the clock to toggle every 1 time units 
    initial 
      begin	
-	clock = 1'b1;
-	forever #5 clock = ~clock; // to toggle the clock every 5 time units. This creates a clock with a 10 time-unit period.
+	clk = 1'b1;
+	forever #10 clk = ~clk; // to toggle the clock every 10 time units. This creates a clock with a 10 time-unit period.
      end 
    
    initial 
      begin      
-	     #0 
+	     #0 we3 = 1'b0;
+       #0 ra1 = 5'h5;
+       #0 ra2 = 5'h8;
+       #0 wd3 = 32'hdead_beef;
+       #0 wa3 = 5'h5;
+       #20 we3 = 1'b1; // at time 20
+       #40 we3 = 1'b0; // at time 40
+       #10 ra1 = 5'h5;
+
+       
 
 
      end
