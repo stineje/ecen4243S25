5f2dd9af296b5bbf2c0ea0c264f3cb7ee7d3137d
James Stine <james.stine@okstate.edu>
2025-02-18 20:33:49 -0600

    fix xor in riscv_single.sv

--- lab2/xor/riscv_single.sv
+++ lab2/xor/riscv_single.sv
@@ -321,12 +321,12 @@ module alu (input  logic [31:0] a, b,
 
    always_comb
      case (alucontrol)
-       3'b000:  result = sum;          // add
-       3'b001:  result = sum;          // subtract
-       3'b010:  result = a & b;        // and
-       3'b011:  result = a | b;        // or
-       3'b101:  result = sum[31] ^ v;  // slt       
-       3'b100:  result = a ^ condinvb; // xor
+       3'b000:  result = sum;         // add
+       3'b001:  result = sum;         // subtract
+       3'b010:  result = a & b;       // and
+       3'b011:  result = a | b;       // or
+       3'b101:  result = sum[31] ^ v; // slt       
+       3'b100:  result = a ^ b;       // xor
        default: result = 32'bx;
      endcase
 
@@ -355,4 +355,3 @@ module regfile (input  logic        clk,
    assign rd2 = (a2 != 0) ? rf[a2] : 0;
    
 endmodule // regfile
-
